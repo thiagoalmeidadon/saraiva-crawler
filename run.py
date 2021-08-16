727948f5aea62ae98b0476a0fb88f043af344f85
Thiago Silva Almeida <thiago.almeida.ext@decode.buzz>
2021-08-16 17:12:52 -0300

    Implementando selenium no método get_detalhes()

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
+# ----------------------------
+# author: Thiago Almeida
+# e-mail: thiagodons@gmail.com
+# year: 2021
+# ----------------------------
 import requests 
 from bs4 import BeautifulSoup
 from requests.sessions import Request
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
 #definindo url e uri livro 
 def get_http(url, nome_livro):
@@ -8,55 +16,48 @@ def get_http(url, nome_livro):
     print(nome_livro)
     url = "{0}{1}".format(url, nome_livro)
     print(url)
+    e = input("teste")
 
     try :
         return requests.get(url)
-    
     except (requests.exceptions.HTTPError, requests.exceptions.RequestException,
             requests.exceptions.ConnectionError, requests.exceptions.Timeout ) as e :
         print(str(e))
         pass
-    
     except Exception as e :
         raise 
 
-
 def get_detalhes(produto_url):
-
-    try :
-        return requests.get(produto_url)
-    
-    except (requests.exceptions.RequestException) as e :
-        pass
+    try :     
+        browser = webdriver.Chrome('./chromedriver')
+        browser.get(produto_url)
+        time.sleep(25)
+        #request = requests.get(produto_url)
+        #soup = BeautifulSoup(request.text, 'lxml')
+        #detalhes = soup.find_all('div', {'id':'descricao'})
+        #for detalhe in detalhes:
+        #    print(detalhe)
+        #return requests.get(produto_url)
+    except Exception as e :
+        print(f"erro aqui {e}")
 
 def get_url_prod(conteudo):
-
     soup = BeautifulSoup(conteudo, 'lxml')
     prods = soup.find_all('div', {'class': 'product _prdv'})
-
     produtos = []
-    
     for prod in prods :
         info_prod = [prod.a.get('href'), prod.h3.a.string]
         produtos.append(info_prod)
-        print(info_prod[0])
-        r = get_detalhes(info_prod[0])
-        #if r:
+        get_detalhes(info_prod[0])
+        #if request:
         #    info_prod.append() 
-    
-    
-    return produtos
-
-
+    #return produtos
 
 
 if __name__ == '__main__':
-
     url = 'https://www.saraiva.com.br/'
     nome_livro = input('Digite o nome do livro: ')
-
     request = get_http(url, nome_livro)
-
     if request :
         print(request.status_code)
         print(get_url_prod(request.text))
