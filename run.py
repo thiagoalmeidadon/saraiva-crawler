de448ff81c19cddbadd6a6d935b79ead7c223214
Thiago Silva Almeida <thiago.almeida.ext@decode.buzz>
2021-08-17 19:12:02 -0300

    Definindo os métodos para baixar detalhes dos produtos, imprimindo resultados

--- run.py
+++ run.py
@@ -10,7 +10,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-#definindo url e uri livro 
 def get_http(url, nome_livro):
     nome_livro = nome_livro.replace(' ', '%20')
     print(nome_livro)
@@ -29,15 +28,15 @@ def get_detalhes(produto_url):
     try :     
         browser = webdriver.Chrome('./chromedriver')
         browser.get(produto_url)
-        time.sleep(10)
+        time.sleep(5)
         browser.refresh()
-        time.sleep(10)
+        time.sleep(5)
         soup = BeautifulSoup(browser.page_source, 'lxml')
         browser.quit()
-        detalhes = soup.find_all('div', {'id':'descricao'})
-        for detalhe in detalhes:
-            print(detalhe)
-        return requests.get(produto_url)
+        descricao = soup.select('#descricao')[0].text
+        preco = soup.select('.price-destaque')[0].text
+        ano_ed = soup.find(id='receba').div.table.tbody.tr.find_next_sibling('tr').td.find_next_sibling('td').string
+        return [descricao, preco, ano_ed]
     except Exception as e :
         print(f"erro aqui {e}")
 
@@ -47,24 +46,24 @@ def get_url_prod(conteudo):
     produtos = []
     for prod in prods :
         info_prod = [prod.a.get('href'), prod.h3.a.string]
-        produtos.append(info_prod)
-        get_detalhes(info_prod[0])
-        #if request:
-        #    info_prod.append() 
-    #return produtos
-
+        detalhes = get_detalhes(info_prod[0])
+        produtos.append(info_prod + detalhes)
+    return produtos
 
 if __name__ == '__main__':
     url = 'https://www.saraiva.com.br/'
     nome_livro = input('Digite o nome do livro: ')
     request = get_http(url, nome_livro)
     if request :
-        print(request.status_code)
-        print(get_url_prod(request.text))
-
-    #with open('results.html', 'w', encoding='utf-8') as f :
-    #    f.write(r.text)
-
+        lista_produtos = get_url_prod(request.text)
+        for produto in lista_produtos:
+            print('-------------------------------------------------------------------------------')
+            print(f'Livro: {produto[1]} ')
+            print(f'Descrição: {produto[2]} ')
+            print(f'Preço: {produto[3]} ')
+            print(f'Ano de publicação: {produto[4]}')
+            print(f'link para compra: {produto[0]}')
+            print('-------------------------------------------------------------------------------')
 
 
 
